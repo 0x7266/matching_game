ff46978322ee4ac810562998779b3f26d88f5728
unknown <rrrr0ffff@gmail.com>
2023-09-17 00:05:12 -0300

    created grid for cells

--- src/lib/Grid.svelte
+++ src/lib/Grid.svelte
@@ -1,8 +1,17 @@
-<div class="grid">TODO</div>
+<script>
+	import Cell from "./Cell.svelte";
+</script>
+
+<div class="grid">
+	{#each Array(16) as _}
+		<Cell />
+	{/each}
+</div>
 
 <style>
 	.grid {
 		display: grid;
+		height: 100%;
 		grid-template-columns: repeat(4, 1fr);
 		grid-template-rows: repeat(4, 1fr);
 	}

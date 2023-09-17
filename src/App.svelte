55f58a43275ac0f857ca0a7349a918e8b887b6e4
unknown <rrrr0ffff@gmail.com>
2023-09-16 23:43:37 -0300

    added basic container

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
 </script>
 
-<main>
-Hello, World!
+<main class="game">
+	<div class="info">a</div>
+	<div class="grid">s</div>
+	<div class="info">a</div>
 </main>
 
 <style>
+	.game {
+		display: grid;
+		place-content: center;
+	}
+	.info {
+		background-color: rgb(70, 11, 70);
+		height: 10vmin;
+		width: 80vmin;
+	}
+	.grid {
+		background-color: #704585;
+		height: 80vmin;
+		width: 80vmin;
+	}
 </style>

438018c7c1a8b5c23156348f8a766048e8a1f141
unknown <rrrr0ffff@gmail.com>
2023-09-18 20:03:15 -0300

    cell style

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -2,10 +2,19 @@
 	export let emoji;
 </script>
 
-<div class="cell">{emoji}</div>
+<div class="cell">
+	<span>{emoji}</span>
+</div>
 
 <style>
 	.cell {
+		align-items: center;
+		display: flex;
 		background-color: #b675c7;
+		justify-content: center;
+	}
+
+	span {
+		font-size: 6em;
 	}
 </style>

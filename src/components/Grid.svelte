02d2f38ab10fec16c82cf1663e5d0ca930926923
unknown <rrrr0ffff@gmail.com>
2023-09-19 20:07:48 -0300

    started writing game logic

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -2,11 +2,22 @@
 	import Cell from "./Cell.svelte";
 
 	export let grid: string[];
+
+	let card_1 = -1;
+	let card_2 = -1;
 </script>
 
 <div class="grid">
-	{#each grid as emoji}
-		<Cell {emoji} />
+	{#each grid as emoji, index}
+		<Cell
+			{emoji}
+			on:click={() => {
+				if (card_1 === -1 && card_2 === -1) {
+					card_1 = index;
+				}
+			}}
+			selected={card_1 === index || card_2 === index}
+		/>
 	{/each}
 </div>
 

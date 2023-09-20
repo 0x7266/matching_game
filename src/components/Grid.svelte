d6732472644b9b7a5edf885b2a19d3ff31aad62b
unknown <rrrr0ffff@gmail.com>
2023-09-19 23:41:46 -0300

    added timeout

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -2,6 +2,7 @@
 	import Cell from "./Cell.svelte";
 
 	export let grid: string[];
+	let reset_timeout: number;
 
 	let card_1 = -1;
 	let card_2 = -1;
@@ -13,13 +14,19 @@
 			{emoji}
 			on:click={() => {
 				if (card_1 === -1 && card_2 === -1) {
+					// clear the timeout when another card is selecting during the current timeout
+					clearTimeout(reset_timeout);
 					card_1 = index;
 				} else if (card_2 === -1) {
 					card_2 = index;
 					if (grid[card_1] === grid[card_2]) {
 						// correct
 					} else {
-						// incorrect
+						// timeout so the cards flip back to initial state after 1200 ms
+						reset_timeout = setTimeout(() => {
+							card_1 = -1;
+							card_2 = -1;
+						}, 1200);
 					}
 				} else {
 					card_1 = index;

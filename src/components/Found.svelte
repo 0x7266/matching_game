befac38e3319fd05c2afbd8ce248711c644143cb
unknown <rrrr0ffff@gmail.com>
2023-09-20 02:45:04 -0300

    styling found

--- src/components/Found.svelte
+++ src/components/Found.svelte
@@ -2,6 +2,20 @@
 	export let found: string[];
 </script>
 
-{#each found as emoji (emoji)}
-	<span>{emoji}</span>
-{/each}
+<div class="found">
+	{#each found as emoji (emoji)}
+		<span>{emoji}</span>
+	{/each}
+</div>
+
+<style>
+	.found {
+		align-items: center;
+		display: flex;
+		justify-content: center;
+		height: 100%;
+	}
+	span {
+		font-size: 3em;
+	}
+</style>

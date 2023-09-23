83afd845c0c7606204d4841a00e8febb1ff2cf1b
unknown <rrrr0ffff@gmail.com>
2023-09-23 16:29:41 -0300

    changed colors

--- src/components/Countdown.svelte
+++ src/components/Countdown.svelte
@@ -7,7 +7,7 @@
 		><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 			<title>Pause</title>
 			<path
-				style="fill: #777; stroke: transparent"
+				style="fill: var(--color-3); stroke: transparent"
 				d="M15,16H13V8H15M11,16H9V8H11M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
 			/>
 		</svg>
@@ -43,7 +43,7 @@
 	}
 
 	.duration {
-		background-color: #ccc;
+		background-color: var(--color-2);
 		flex: 1;
 		width: 10px;
 		position: relative;
@@ -52,7 +52,7 @@
 	}
 
 	.remaining {
-		background-color: #888;
+		background-color: var(--color-3);
 		position: absolute;
 		width: 100%;
 		height: calc(100% * var(--p));

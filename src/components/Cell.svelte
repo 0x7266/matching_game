1c170b7792cabf9dfc4e5ceca7ebfc8aeef8337c
unknown <rrrr0ffff@gmail.com>
2023-09-28 17:59:15 -0300

    added bg

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { get_emoji_svg } from "../lib/get_emoji_svg";
+	import { send } from "../lib/transitions";
 
 	export let emoji: string, selected: boolean, found: boolean;
+
+	$: console.log(found);
 </script>
 
-<button
-	class="cell"
-	on:click
-	disabled={selected || found}
-	class:flipped={selected || found}
->
-	<img alt={emoji} src={get_emoji_svg(emoji)} />
-</button>
+<div class="cell" class:flipped={selected || found}>
+	<button on:click disabled={selected || found} />
+
+	<div class="background" class:found />
+	{#if !found}
+		<img alt={emoji} src={get_emoji_svg(emoji)} out:send={{ key: emoji }} />
+	{/if}
+</div>
 
 <style>
 	.cell {
@@ -40,6 +43,7 @@
 		display: flex;
 		height: 100%;
 		justify-content: center;
+		position: absolute;
 		transform: rotateY(180deg);
 		width: 100%;
 		-webkit-tap-highlight-color: transparent;
@@ -47,6 +51,21 @@
 
 	button:disabled {
 		color: inherit;
+	}
+
+	.background {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: var(--color-1);
+		border: 2px solid var(--color-3);
+		border-radius: 1em;
+		transition: border 0.2s;
+		pointer-events: none;
+	}
+
+	.background.found {
+		border: 2px solid var(--bg-2);
 	}
 
 	.flipped {

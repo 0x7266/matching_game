cac65f631a978eaee39795f8d9a45af663c7ca05
unknown <rrrr0ffff@gmail.com>
2023-09-28 17:59:35 -0300

    added receive

--- src/components/Found.svelte
+++ src/components/Found.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { get_emoji_svg } from "../lib/get_emoji_svg";
+	import { receive } from "../lib/transitions";
 
 	export let found: string[];
 </script>
 
 <div class="found">
 	{#each found as emoji (emoji)}
-		<img src={get_emoji_svg(emoji)} alt={emoji} />
+		<img in:receive={{ key: emoji }} src={get_emoji_svg(emoji)} alt={emoji} />
 	{/each}
 </div>
 

14c1d4d191d832a4fcfc4d5f5dae92d29dcdf205
unknown <rrrr0ffff@gmail.com>
2023-09-30 02:09:20 -0300

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,52 @@
-<script>
+<script lang="ts">
 	import Game from "./components/Game.svelte";
+	import Modal from "./components/Modal.svelte";
+
+	let state: "waiting" | "playing" | "paused" | "won" | "lost" = "waiting";
 </script>
 
+{#if state !== "playing"}
+	<Modal>
+		{#if state === "waiting"}
+			<div class="waiting">
+				<p>CHOOSE A LEVEL</p>
+				<div class="levels">
+					<button>EASY</button>
+					<button>MEDIUM</button>
+					<button>HARD</button>
+				</div>
+			</div>
+		{/if}
+	</Modal>
+{/if}
 <Game />
+
+<style>
+	.waiting {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: 2em;
+
+		& p {
+			color: var(--color-5);
+			font-size: 2em;
+			font-weight: bold;
+		}
+
+		& .levels {
+			display: flex;
+			gap: 1em;
+			width: 100%;
+		}
+
+		& button {
+			background-color: var(--color-6);
+			border: none;
+			border-radius: 0.4em;
+			color: var(--color-5);
+			font-size: 2em;
+			padding: 0.3em 0.7em;
+		}
+	}
+</style>

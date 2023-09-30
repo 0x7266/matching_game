6c6c35f3ce38cc823b6abc7ee6a2ee09835148fc
unknown <rrrr0ffff@gmail.com>
2023-09-30 01:52:17 -0300

    added modal component

---
+++ src/components/Modal.svelte
@@ -0,0 +1,17 @@
+<div class="modal-background">
+	<div class="modal-content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.modal-background {
+		backdrop-filter: blur(10px);
+		display: grid;
+		height: 100%;
+		place-items: center;
+		position: fixed;
+		width: 100%;
+		z-index: 10;
+	}
+</style>

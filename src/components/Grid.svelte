32df23222565ac03405d153561d8be2e644937bb
unknown <rrrr0ffff@gmail.com>
2023-10-01 00:33:40 -0300

    fixed game logic

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -14,24 +14,25 @@
 {#each grid as emoji, index}
 	<Cell
 		on:click={() => {
-			if (card_1 === -1 && card_2 === -1) {
+			if (card_1 > -1 && card_2 > -1) {
 				// clear the timeout when another card is selecting during the current timeout
 				clearTimeout(reset_timeout);
 				card_1 = index;
-			} else if (card_2 === -1) {
+				card_2 = -1;
+			} else if (card_1 > -1) {
 				card_2 = index;
 				if (grid[card_1] === grid[card_2]) {
-					dispatch("found", { emoji });
+					// correct - remove from grid
+					found = [...found, grid[card_1]];
+					dispatch("found", { emoji: grid[card_1] });
 				} else {
-					// timeout so the cards flip back to initial state after 1200 ms
+					// incorrect - reset after timeout so the cards flip back to initial state after 1200 ms
 					reset_timeout = setTimeout(() => {
-						card_1 = -1;
-						card_2 = -1;
+						card_1 = card_2 - 1;
 					}, 1200);
 				}
 			} else {
 				card_1 = index;
-				card_2 = -1;
 			}
 		}}
 		{emoji}
